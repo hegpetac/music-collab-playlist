b44a08ddd93b667c0618cdf41e24ee1374aed4e6
hegpetac <hegeduspeti2000@gmail.com>
2025-10-04 06:35:07 +0200

    Init Spring Boot starter

---
+++ music-collab-playlist-be/src/main/java/hu/hegpetac/music_collab_playlist_be/MusicCollabPlaylistBeApplication.java
@@ -0,0 +1,13 @@
+package hu.hegpetac.music_collab_playlist_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicCollabPlaylistBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicCollabPlaylistBeApplication.class, args);
+	}
+
+}
---
+++ music-collab-playlist-be/src/test/java/hu/hegpetac/music_collab_playlist_be/MusicCollabPlaylistBeApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.hegpetac.music_collab_playlist_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicCollabPlaylistBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
