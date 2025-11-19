aacd039e64871871681dcc33f730873c1d7dd006
hegpetac <hegeduspeti2000@gmail.com>
2025-11-19 08:22:10 +0100

    Configure docker image pull for npm

--- do-all.sh
+++ do-all.sh
@@ -1,9 +1,13 @@
+set -a
+. ./.env
+set +a
+
 cd music-collab-playlist-be
 mvn clean install -DskipTests
-docker build -t music-collab-playlist-be .
+docker build  -t music-collab-playlist-be .
 cd ..
 
 cd music-collab-playlist-fe
 ng build
-docker build -t music-collab-playlist-fe .
+docker build --build-arg GITHUB_TOKEN="$NPM_TOKEN" -t music-collab-playlist-fe .
 cd ..
